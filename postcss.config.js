fd4d2351ca04538ea221cf7daf984176933a8ffc
sartaj10 <ssbaveja@es.net>
2026-09-08 12:42:49 -0700

    Fix config

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,10 @@
+const path = require("path");
+
 module.exports = {
 	plugins: {
+		"postcss-import": {
+			path: [path.resolve(__dirname, "node_modules")],
+		},
 		tailwindcss: {},
 		autoprefixer: {},
 	},
