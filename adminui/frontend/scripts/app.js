1e48476652e0c8d75fa7ec5999feec3273ec94c1
bradojev <branko@itcs.hr>
2026-09-11 07:31:45 +0200

    Add metadata records view

--- adminui/frontend/scripts/app.js
+++ adminui/frontend/scripts/app.js
@@ -16,6 +16,7 @@ const ROUTES = {
 	"/define-table": "/pages/clickhouse-table.html",
 	"/metadata-manager": "/pages/manage-metadata.html",
 	"/metadata-saved": "/pages/metadata-saved.html",
+	"/metadata-records": "/pages/metadata-records.html",
 	"/define-pipeline": "/pages/define-pipelines.html",
 	"/tables": "/pages/tables.html",
 	"/pipelines": "/pages/pipelines.html",
