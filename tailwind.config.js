6fc115518af8c32a909624566a334f64f9471156
Johanna Lee <johyunjilee@gmail.com>
2026-09-18 11:25:34 -0400

    update more vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,24 +5,27 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
+				primary: "var(--primary)",
+				secondary: "var(--secondary)",
+				tertiary: "var(--tertiary)",
+
 				background: "var(--background)",
 				surface1: "var(--surface-1)",
 				surface2: "var(--surface-2)",
-				"surface-menu": "var(--surface-menu)",
-				"active-menu": "var(--menu-active)",
+
 				copy: "var(--copy)",
 				"copy-alt": "var(--copy-alt)",
+				"copy-alt-2": "var(--copy-alt-2)",
 				shadow: "var(--shadow)",
-				primary: "var(--primary)",
-				secondary: "var(--secondary)",
-				accent: "var(--accent)",
+
 				success: "var(--success)",
 				error: "var(--error)",
+				accent: "var(--accent)",
+
+				"surface-menu": "var(--surface-menu)",
+				"active-menu": "var(--menu-active)",
 			},
 			fontFamily: {
-				// display: ["Signika", "sans-serif"],
-				// sans: ["Source Sans 3", "sans-serif"],
-				// mono: ["Source Code Pro", "monospace"],
 				display: ["Hanuman", "serif"],
 				sans: ["Inter", "sans-serif"],
 			},
